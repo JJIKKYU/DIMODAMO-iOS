a2fbfd480e728b05ff553a53ef1f065d9028f419
Jin Gyun, Jeong <jjinggu96@gmail.com>
2020-01-26 19:48:05 +0900

    add: study #5 - flutter
    flutter의 Widget 사용 방법, 특히 Row와 Column 그리고 Expanded를 활용한 레이아웃에 대해 중점적으로 스터디함.

---
+++ 0_study/Flutter/Section7/dicee-flutter/lib/main.dart
@@ -0,0 +1,64 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.blue,
+        appBar: AppBar(
+          title: Text('Dicee'),
+          backgroundColor: Colors.blue,
+        ),
+        body: DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 5;
+  int rightDiceNumber = 1;
+
+  void changeDiceFace() {
+    setState(() {
+      leftDiceNumber = Random().nextInt(6) + 1;
+      rightDiceNumber = Random().nextInt(6) + 1;
+      print("leftDiceNumber = $leftDiceNumber");
+      print("rightDiceNumber = $rightDiceNumber");
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                changeDiceFace();
+              },
+              child: Image.asset('images/dice$leftDiceNumber.png'),
+            ),
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                changeDiceFace();
+              },
+              padding: const EdgeInsets.all(16.0),
+              child: Image.asset('images/dice$rightDiceNumber.png'),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
