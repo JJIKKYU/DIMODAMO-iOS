30d563d1e28ba49e907414d08b46d81977f8d04a
Jin Gyun, Jeong <jjinggu96@gmail.com>
2020-01-29 11:48:31 +0900

    add: study #9 - flutter
    페이지간 이동하는 방법 및 레이아웃 구성 등에 대해서 학습함

---
+++ 0_study/Flutter/Section12/bmi-calculator/lib/constants.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+
+const kTextStyle = TextStyle(
+  fontSize: 18.0,
+  color: Colors.white,
+);
+
+const kNumberTextStyle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.w900,
+);
+
+const kIconSize = 80.0;
+const kSizedBoxHeight = 15.0;
+
+const kBottomContainerHeight = 80.0;
+const kActiveCardColour = Color(0xffffd33b);
+const kInactiveCardColour = Color(0xFF0063ac);
+const kBottomContainerColour = Color(0xFFeb1555);
+
+const kHeightMinValue = 120.0;
+const kHeightMaxValue = 220.0;
+
+const kLargeButtonTextStyle = TextStyle(
+  fontSize: 25.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kTitleTextStyle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kResultTextStyle = TextStyle(
+  color: Color(0xFF24D876),
+  fontSize: 22.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kBMITextStyle = TextStyle(
+  fontSize: 100.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kBodyTextStyle = TextStyle(
+  fontSize: 22.0,
+);
