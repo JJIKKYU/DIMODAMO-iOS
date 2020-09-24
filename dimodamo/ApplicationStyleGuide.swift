e4d7fffe19f95c2c8a735eb23e075dda6615ceee
Jin Gyun, Jeong <jjinggu96@gmail.com>
2020-09-24 10:34:11 +0900

    UPDATE : Register Birth Textfiled

--- dimodamo/ApplicationStyleGuide.swift
+++ dimodamo/ApplicationStyleGuide.swift
@@ -168,3 +168,36 @@ class AppStyleGuide {
         navigationController.navigationBar.tintColor = UIColor.appColor(.system)
     }
 }
+
+
+//MARK: - UILABEL Kerning
+
+extension UILabel {
+
+    @IBInspectable var kerning: Float {
+        get {
+            var range = NSMakeRange(0, (text ?? "").count)
+            guard let kern = attributedText?.attribute(NSAttributedString.Key.kern, at: 0, effectiveRange: &range),
+                let value = kern as? NSNumber
+                else {
+                    return 0
+            }
+            return value.floatValue
+        }
+        set {
+            var attText:NSMutableAttributedString
+
+            if let attributedText = attributedText {
+                attText = NSMutableAttributedString(attributedString: attributedText)
+            } else if let text = text {
+                attText = NSMutableAttributedString(string: text)
+            } else {
+                attText = NSMutableAttributedString(string: "")
+            }
+
+            let range = NSMakeRange(0, attText.length)
+            attText.addAttribute(NSAttributedString.Key.kern, value: NSNumber(value: newValue), range: range)
+            self.attributedText = attText
+        }
+    }
+}
--- dimodamo/Pages/Register/Birth/RegisterBirthViewController.swift
+++ dimodamo/Pages/Register/Birth/RegisterBirthViewController.swift
@@ -8,19 +8,48 @@
 
 import UIKit
 
-class RegisterBirthViewController: UIViewController {
+class RegisterBirthViewController: UIViewController, UITextFieldDelegate {
 
-    @IBOutlet weak var birthTextField: UITextField!
     @IBOutlet weak var nextBtn: UIButton!
+    
+    @IBOutlet weak var yearTextField: UITextField!
+    @IBOutlet weak var monthTextField: UITextField!
+    @IBOutlet weak var dayTextField: UITextField!
+    
     
     private var datePicker: UIDatePicker!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        datePickerDesign()
-        AppStyleGuide.systemBtnRadius16(btn: nextBtn, isActive: false)
+        registerBirthDesign()
+        
+        
+        // 4글자가 넘어갈 경우 자동으로 다음 텍스트 필드로 넘어갈 수 있도록
+        yearTextField?.addTarget(self, action: #selector(RegisterBirthViewController.textFieldDidChange(_:)), for: UIControl.Event.editingChanged)
+        monthTextField?.addTarget(self, action: #selector(RegisterBirthViewController.textFieldDidChange(_:)), for: UIControl.Event.editingChanged)
+        dayTextField?.addTarget(self, action: #selector(RegisterBirthViewController.textFieldDidChange(_:)), for: UIControl.Event.editingChanged)
+
     }
+    
+    @objc func textFieldDidChange(_ textField: UITextField) {
+        if textField == yearTextField {
+            if (textField.text?.count)! >= 4 {
+                monthTextField?.becomeFirstResponder()
+            }
+        }
+        else if textField == monthTextField {
+            if (textField.text?.count)! >= 2 {
+                dayTextField?.becomeFirstResponder()
+            }
+        }
+        else if textField == dayTextField {
+            if (textField.text?.count)! >= 2 {
+                dayTextField.resignFirstResponder()
+            }
+        }
+    }
+    
 
     @IBAction func pressedNextBtn(_ sender: Any) {
         performSegue(withIdentifier: "InputGender", sender: sender)
@@ -40,26 +69,41 @@ class RegisterBirthViewController: UIViewController {
 
 
 extension RegisterBirthViewController {
-    func datePickerDesign() {
-        // DatePicker 정의
-        datePicker = UIDatePicker.init(frame: CGRect(x: 0, y: 0, width: view.bounds.size.width, height: 400))
-        datePicker.preferredDatePickerStyle = .wheels
-        datePicker.datePickerMode = .date
-        datePicker.addTarget(self, action: #selector(self.dateChanged), for: .allEvents)
-        birthTextField.inputView = datePicker
-        
-        // 상단 툴바 Done 버튼 정의
-        let doneButton = UIBarButtonItem.init(title: "Done", style: .done, target: self, action: #selector(self.datePickerDone))
-        let toolBar = UIToolbar.init(frame: CGRect(x: 0, y: 0, width: view.bounds.size.width, height: 44))
-        toolBar.setItems([UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.flexibleSpace, target: nil, action: nil), doneButton], animated: true)
-        birthTextField.inputAccessoryView = toolBar
+    
+    func registerBirthDesign() {
+        designTextfieldTextStyle()
+        AppStyleGuide.systemBtnRadius16(btn: nextBtn, isActive: false)
     }
     
-    @objc func datePickerDone() {
-        birthTextField.resignFirstResponder()
+    func designTextfieldTextStyle() {
+        let spacing: CGFloat = 14.0
+        
+        yearTextField.defaultTextAttributes.updateValue(spacing, forKey: NSAttributedString.Key.kern)
+        monthTextField.defaultTextAttributes.updateValue(spacing, forKey: NSAttributedString.Key.kern)
+        dayTextField.defaultTextAttributes.updateValue(spacing, forKey: NSAttributedString.Key.kern)
     }
-
-    @objc func dateChanged() {
-        birthTextField.text = "\(datePicker.date)"
+    
+    
+    func datePickerDesign() {
+        // DatePicker 정의
+//        datePicker = UIDatePicker.init(frame: CGRect(x: 0, y: 0, width: view.bounds.size.width, height: 400))
+//        datePicker.preferredDatePickerStyle = .wheels
+//        datePicker.datePickerMode = .date
+//        datePicker.addTarget(self, action: #selector(self.dateChanged), for: .allEvents)
+//        birthTextField.inputView = datePicker
+//        
+//        // 상단 툴바 Done 버튼 정의
+//        let doneButton = UIBarButtonItem.init(title: "Done", style: .done, target: self, action: #selector(self.datePickerDone))
+//        let toolBar = UIToolbar.init(frame: CGRect(x: 0, y: 0, width: view.bounds.size.width, height: 44))
+//        toolBar.setItems([UIBarButtonItem(barButtonSystemItem: UIBarButtonItem.SystemItem.flexibleSpace, target: nil, action: nil), doneButton], animated: true)
+//        birthTextField.inputAccessoryView = toolBar
     }
+    
+//    @objc func datePickerDone() {
+//        birthTextField.resignFirstResponder()
+//    }
+//
+//    @objc func dateChanged() {
+//        birthTextField.text = "\(datePicker.date)"
+//    }
 }
