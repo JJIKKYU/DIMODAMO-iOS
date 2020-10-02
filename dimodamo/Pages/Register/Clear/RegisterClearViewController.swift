e52e87eabf7c0e3f8d2c3ffb8090145da0798fab
Jin Gyun, Jeong <jjinggu96@gmail.com>
2020-10-02 19:09:26 +0900

    ADD : Register Clear View Controller & StoryBoard

---
+++ dimodamo/Pages/Register/Clear/RegisterClearViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RegisterClearViewController.swift
+//  dimodamo
+//
+//  Created by JJIKKYU on 2020/10/02.
+//  Copyright © 2020 JJIKKYU. All rights reserved.
+//
+
+import UIKit
+
+class RegisterClearViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
