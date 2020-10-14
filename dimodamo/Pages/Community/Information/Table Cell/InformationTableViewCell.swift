566ab9a59fe9ab245ba1585d70ca84d8b596c1dc
Jin Gyun, Jeong <jjinggu96@gmail.com>
2020-10-15 04:02:30 +0900

    ADD : Community Main Screen Implement

---
+++ dimodamo/Pages/Community/Information/Table Cell/InformationTableViewCell.swift
@@ -0,0 +1,54 @@
+//
+//  InformationTableViewCell.swift
+//  dimodamo
+//
+//  Created by JJIKKYU on 2020/10/15.
+//  Copyright © 2020 JJIKKYU. All rights reserved.
+//
+
+import UIKit
+
+class InformationTableViewCell: UITableViewCell {
+
+    
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet var tags: [UILabel]!
+    @IBOutlet weak var profile: UIImageView!
+    @IBOutlet weak var nickName: UILabel!
+    
+    @IBOutlet weak var scrapCnt: UILabel!
+    @IBOutlet weak var commnetCnt: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        viewDesign()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
+
+extension InformationTableViewCell {
+    func viewDesign() {
+        tagDesign()
+    }
+    
+    func tagDesign(){
+        
+        // 태그 내부 글자 수에 맞춰서 width, height 재설정
+        for tag in tags {
+            let width: Int = (Int(tag.text!.count) * 10) + 20
+            let height: Int = 20
+            tag.translatesAutoresizingMaskIntoConstraints = false
+            tag.widthAnchor.constraint(equalToConstant: CGFloat(width)).isActive = true
+            tag.heightAnchor.constraint(equalToConstant: CGFloat(height)).isActive = true
+            
+            tag.layer.masksToBounds = true
+            tag.layer.cornerRadius = 10
+        }
+    }
+}
--- dimodamo/Pages/Community/Main/CommunityMainViewController.swift
+++ dimodamo/Pages/Community/Main/CommunityMainViewController.swift
@@ -9,33 +9,83 @@
 import UIKit
 import WebKit
 
-class CommunityMainViewController: UIViewController, WKUIDelegate, WKNavigationDelegate {
+class CommunityMainViewController: UIViewController {
     
-    @IBOutlet var webView: WKWebView!
+    private let imageView = UIImageView(image: UIImage(named: "searchIcon"))
+    @IBOutlet weak var tableView: UITableView!
     
-    func goWeb(postfix: String) -> () {
-        let url = URL(string: "http://dimodamo.com")
-        let request = URLRequest(url: url!)
-        webView.load(request)
-    }
-        
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        goWeb(postfix: "scale")
         
+        tableView.delegate = self
+        tableView.dataSource = self
+        settingTableView()
+        
+        setupUI()
     }
     
     
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
+    private func setupUI() {
+        
+        // Initial setup for image for Large NavBar state since the the screen always has Large NavBar once it gets opened
+        guard let navigationBar = self.navigationController?.navigationBar else { return }
+        navigationBar.addSubview(imageView)
+        imageView.clipsToBounds = true
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            imageView.rightAnchor.constraint(equalTo: navigationBar.rightAnchor, constant: -Const.ImageRightMargin),
+            imageView.bottomAnchor.constraint(equalTo: navigationBar.bottomAnchor, constant: -Const.ImageBottomMarginForLargeState),
+            imageView.heightAnchor.constraint(equalToConstant: Const.ImageSizeForLargeState),
+            imageView.widthAnchor.constraint(equalTo: imageView.heightAnchor)
+        ])
     }
-    */
+    
+}
 
+extension CommunityMainViewController: UITableViewDataSource, UITableViewDelegate {
+    func settingTableView() {
+        tableView.rowHeight = 140
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "informationCell", for: indexPath)
+        
+        return cell
+    }
+    
+    
+}
+
+
+/// WARNING: Change these constants according to your project's design
+private struct Const {
+    /// Image height/width for Large NavBar state
+    static let ImageSizeForLargeState: CGFloat = 18
+    /// Margin from right anchor of safe area to right anchor of Image
+    static let ImageRightMargin: CGFloat = 16
+    /// Margin from bottom anchor of NavBar to bottom anchor of Image for Large NavBar state
+    static let ImageBottomMarginForLargeState: CGFloat = 12
+    /// Margin from bottom anchor of NavBar to bottom anchor of Image for Small NavBar state
+    static let ImageBottomMarginForSmallState: CGFloat = 6
+    /// Image height/width for Small NavBar state
+    static let ImageSizeForSmallState: CGFloat = 18
+    /// Height of NavBar for Small state. Usually it's just 44
+    static let NavBarHeightSmallState: CGFloat = 44
+    /// Height of NavBar for Large state. Usually it's just 96.5 but if you have a custom font for the title, please make sure to edit this value since it changes the height for Large state of NavBar
+    static let NavBarHeightLargeState: CGFloat = 96.5
 }
