fe1664ae6ecfd719d34918cbba0e9222c80308f8
Jin Gyun, Jeong <jjinggu96@gmail.com>
2020-10-18 18:17:45 +0900

    ADD : Article Model

---
+++ dimodamo/Pages/Community/Article/ArticleModel.swift
@@ -0,0 +1,20 @@
+//
+//  Article.swift
+//  dimodamo
+//
+//  Created by JJIKKYU on 2020/10/18.
+//  Copyright © 2020 JJIKKYU. All rights reserved.
+//
+
+import Foundation
+
+// 아티클에 필요한 모델
+class Article {
+    var category: Category = .magzine
+}
+
+
+// 아티클 카테고리
+enum Category {
+    case magzine
+}
