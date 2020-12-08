f2b0f6dcd51e513fd6176e9814b5b54c1c1c8dfc
Jin Gyun, Jeong <jjinggu96@gmail.com>
2020-12-09 04:44:54 +0900

    UPDATE : Information Comment Cloud Functions

--- 1_FirebaseFunction/FirebaseDocument/functions/index.js
+++ 1_FirebaseFunction/FirebaseDocument/functions/index.js
@@ -85,21 +85,61 @@ exports.documentCommentCounter_artboard = functions.firestore
 
 // Information (디모레이어) 에 댓글달 경우에 카운트 증가
 exports.documentCommentCounter_layer = functions.firestore
-.document('hongik/information/comments/{comment_id}')
-.onCreate((snap, context) => {
-    const newValue = snap.data();
+    .document('hongik/information/comments/{comment_id}')
+    .onCreate(async (snap, context) => {
+        try {
+            const newValue = snap.data();
 
-    const commentId = context.params.comment_id;
-    const postId = newValue.post_id;
+            // 작성한 댓글 UID
+            const commentId = context.params.comment_id;
+            // 댓글 작성자 닉네임
+            const commentNickname = newValue.nickname;
+            // 댓글 내용
+            const commentDesc = newValue.comment
+            // 댓글이 달린 게시글 UID
+            const postId = newValue.post_id;
 
-    console.log(postId + ": postID입니다.");
-    console.log(commentId + " : commentID입니다.");
+    
+            console.log(postId + ": postID입니다.");
+            console.log(commentId + " : commentID입니다.");
+    
+            const postDocRefSnapshot = await admin.firestore().collection('hongik/information/posts').doc(postId).get();
+            const data = postDocRefSnapshot.data();
 
-    const postDocRef = admin.firestore().collection('hongik/information/posts').doc(postId);
-    return postDocRef.get().then(snap => {
-        const commentCount = snap.data().comment_count + 1;
-        console.log("수정될 코멘트 카운트 : " + commentCount);
+            // 작성자 UID -> 알림 보내야할 목표 대상
+            const postUserId = data.user_id;
+            console.log("postUserID : " + postUserId);
+            console.log("수정될 코멘트 카운트 : " + (data.comment_count + 1));
 
-        return postDocRef.update( {comment_count : commentCount});
-    })
-});
+            // 댓글 작성자와 게시글 작성자가 같지 않을 때만 알림 가도록
+            if (postUserId !== commentId) {
+                // 작성자의 FCM 토큰을 불러옴
+                const fcmIdDocRefSnapshot = await admin.firestore().collection('FcmId').doc(postUserId).get();
+                const fcmIdData = fcmIdDocRefSnapshot.data();
+                console.log("fcmIdData : " + fcmIdData);
+                // 정상적으로 FCM 토큰을 가지고 왔을 때만 알림을 보내도록 설정
+                if (fcmIdData) {
+                    
+                    const fcmToken = fcmIdData.FCM;
+                    console.log("fcmToken : " + fcmToken);
+    
+                    // 알림 내용
+                    const payload = {
+                        notification: {
+                        title: '작성하신 글에 새 댓글이 달렸습니다!',
+                        body: `${commentNickname} : ${commentDesc}`
+                        }
+                    };
+    
+                    // 알림 발송
+                    const response = await admin.messaging().sendToDevice(fcmToken, payload);
+                }
+            }
+
+            // 게시글 코멘트 +1
+            admin.firestore().collection('hongik/information/posts').doc(postId).update( {comment_count : data.comment_count + 1});
+
+        } catch (error) {
+            console.log("Error : ", error);
+        }
+    });
