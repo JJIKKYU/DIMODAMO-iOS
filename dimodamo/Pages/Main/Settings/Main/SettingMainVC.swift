98511618cb9e3b5887f27e09a13accafefd16bb8
Jin Gyun, Jeong <jjinggu96@gmail.com>
2020-12-13 01:30:16 +0900

    UPDATE : Settings Screen

--- dimodamo/Pages/Main/Settings/Main/SettingMainVC.swift
+++ dimodamo/Pages/Main/Settings/Main/SettingMainVC.swift
@@ -8,12 +8,37 @@
 
 import UIKit
 
-class SettingMainVC: UIViewController {
+import RxSwift
+import RxCocoa
 
+class SettingMainVC: UIViewController {
+    
+    let viewModel = SettingMainViewModel()
+    var disposeBag = DisposeBag()
+
+    @IBOutlet weak var settingProfileImageView: UIImageView!
+    @IBOutlet weak var settingNicknameLabel: UILabel!
+    @IBOutlet weak var settingCreatedAtLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        /*
+         프로필 세팅
+         */
+        viewModel.mySettingProfile
+            .observeOn(MainScheduler.instance)
+            .subscribe(onNext: { [weak self] value in
+                guard let dpti: String = value.dpti,
+                    let nickname: String = value.nickname,
+                    let createdAt: String = value.registerDate else {
+                    return
+                }
+                self?.settingProfileImageView.image = UIImage(named: "Profile_\(dpti)")
+                self?.settingNicknameLabel.text = nickname
+                self?.settingCreatedAtLabel.text = createdAt
+            })
+            .disposed(by: disposeBag)
     }
     
 
@@ -27,4 +52,15 @@ class SettingMainVC: UIViewController {
     }
     */
 
+    // MARK: - 버튼 바인딩
+    
+    /*
+     계정
+     */
+    @IBAction func pressedBlockedUser(_ sender: Any) {
+        performSegue(withIdentifier: "BlockedUserVC", sender: sender)
+    }
+    
+    @IBAction func pressedLogout(_ sender: Any) {
+    }
 }
---
+++ dimodamo/Pages/Main/Settings/Main/SettingModel.swift
@@ -0,0 +1,15 @@
+//
+//  SettingModel.swift
+//  dimodamo
+//
+//  Created by JJIKKYU on 2020/12/13.
+//  Copyright © 2020 JJIKKYU. All rights reserved.
+//
+
+import Foundation
+
+struct SettingUserInformation {
+    var dpti: String?
+    var nickname: String?
+    var registerDate: String?
+}
